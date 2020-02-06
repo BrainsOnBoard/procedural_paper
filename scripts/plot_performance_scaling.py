8f68aff1eeb4841dc28f07225f3e2ca5273e066c
neworderofjamie <J.C.Knight@sussex.ac.uk>
2020-02-06 17:59:19 +0000

    Improved plotting

--- scripts/plot_performance_scaling.py
+++ scripts/plot_performance_scaling.py
@@ -1,7 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 import seaborn as sns
 import plot_settings
+
+from six import iterkeys, itervalues
 
 markers = ["o", "^", "s"]
 
@@ -17,25 +20,39 @@ connectivity_markers = {}
 
 # Loop through names of columns containing experiment data
 for n in data.dtype.names[2:]:
+    # Extract device and connectivity name from column name
     device_name, connectivity_name = (f.strip(" ") for f in n.split("-"))
 
+    # Find colour corresponding to device
     if device_name in device_colours:
         colour = device_colours[device_name]
     else:
         colour = pal[len(device_colours)]
         device_colours[device_name] = colour
 
+    # Find marker correspond to connectivity
     if connectivity_name in connectivity_markers:
         marker = connectivity_markers[connectivity_name]
     else:
         marker = markers[len(connectivity_markers)]
         connectivity_markers[connectivity_name] = marker
 
+    # Plot
     axis.plot(data["Num neurons"], data[n], color=colour, marker=marker)
 
 axis.set_xlabel("Number of neurons")
 axis.set_ylabel("Simulation time [s]")
 axis.set_xscale("log")
 axis.set_yscale("log")
+
+legend_actors = []
+legend_text = []
+legend_actors.extend(Line2D([], [], color=c) for c in itervalues(device_colours))
+legend_text.extend(d for d in iterkeys(device_colours))
+legend_actors.extend(Line2D([], [], marker=m, linestyle="none") for m in itervalues(connectivity_markers))
+legend_text.extend(c for c in iterkeys(connectivity_markers))
+
+fig.legend(legend_actors, legend_text, ncol=2, loc="lower center")
+
 plt.tight_layout(pad=0)
 plt.show()
