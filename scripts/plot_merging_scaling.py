9f57a531d15382f22a82d6faeee3cd24aab4bc8e
neworderofjamie <neworderofjamie@gmail.com>
2020-08-13 17:49:36 +0100

    reviewer one minor comments and typos

--- scripts/plot_merging_scaling.py
+++ scripts/plot_merging_scaling.py
@@ -12,12 +12,14 @@ fig, axes = plt.subplots(2, 2, sharex="col", frameon=False,
                                   5.0 * plot_settings.cm_to_inches))
 
 # Plot compile time
+plot_style = {"marker": "o"}
 compile_time_axis = axes[0,0]
 compile_time_axis.set_title("A", loc="left")
-latest_actor = compile_time_axis.plot(data[:,0], data[:,2])[0]
-release_actor = compile_time_axis.plot(data[:,0], data[:,7])[0]
+latest_actor = compile_time_axis.plot(data[:,0], data[:,2], **plot_style)[0]
+release_actor = compile_time_axis.plot(data[:,0], data[:,7], **plot_style)[0]
 #compile_time_axis.set_xlabel("Num populations")
 compile_time_axis.set_ylabel("$T_{comp}$ [s]")
+compile_time_axis.set_yticks([0, 1000, 2000])
 compile_time_axis.xaxis.grid(False)
 compile_time_axis.yaxis.grid(False)
 sns.despine(ax=compile_time_axis)
@@ -25,8 +27,8 @@ sns.despine(ax=compile_time_axis)
 # Plot simulation time
 sim_time_axis = axes[0,1]
 sim_time_axis.set_title("B", loc="left")
-sim_time_axis.plot(data[:,0], data[:,1])
-sim_time_axis.plot(data[:,0], data[:,6])
+sim_time_axis.plot(data[:,0], data[:,1], **plot_style)
+sim_time_axis.plot(data[:,0], data[:,6], **plot_style)
 #sim_time_axis.set_xlabel("Num populations")
 sim_time_axis.set_ylabel("$T_{sim}$ [s]")
 sim_time_axis.xaxis.grid(False)
@@ -36,9 +38,9 @@ sns.despine(ax=sim_time_axis)
 # Plot SOL time
 sol_time_axis = axes[1,0]
 sol_time_axis.set_title("C", loc="left")
-sol_time_axis.plot(data[:,0], data[:,3])
+sol_time_axis.plot(data[:,0], data[:,3], **plot_style)
 #axes[1,0].plot(data[:,0], data[:,4], color=pal[0], linestyle="--")
-sol_time_axis.plot(data[:,0], data[:,8])
+sol_time_axis.plot(data[:,0], data[:,8], **plot_style)
 #[1,0].plot(data[:,0], data[:,9], color=pal[1], linestyle="--")
 sol_time_axis.set_xlabel("$N_{pop}$")
 sol_time_axis.set_ylabel("$K_{mem}$ [%]")
@@ -50,8 +52,8 @@ sns.despine(ax=sol_time_axis)
 # Plot instruction stall count
 stall_count_axis = axes[1,1]
 stall_count_axis.set_title("D", loc="left")
-stall_count_axis.plot(data[:,0], data[:,5])
-stall_count_axis.plot(data[:,0], data[:,10])
+stall_count_axis.plot(data[:,0], data[:,5], **plot_style)
+stall_count_axis.plot(data[:,0], data[:,10], **plot_style)
 stall_count_axis.set_xlabel("$N_{pop}$")
 stall_count_axis.set_ylabel("$N_{stall}$")
 stall_count_axis.xaxis.grid(False)
