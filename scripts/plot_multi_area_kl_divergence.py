39589f6a34b35a64e8750b554dfef76c5893e927
neworderofjamie <neworderofjamie@gmail.com>
2020-09-01 11:13:17 +0100

    KL divergence figure

--- scripts/plot_multi_area_kl_divergence.py
+++ scripts/plot_multi_area_kl_divergence.py
@@ -1,4 +1,5 @@
 import numpy as np
+import seaborn as sns
 from glob import glob
 from matplotlib import pyplot as plt
 from scipy.stats import entropy
@@ -6,6 +7,10 @@ from sys import argv
 from os import path
 import plot_settings
 
+def remove_junk(axis):
+    sns.despine(ax=axis, left=True)
+    axis.xaxis.grid(False)
+    
 def calc_kl_divergence(data_path, prefix, populations):
     # Loop through populations
     kls = []
@@ -33,6 +38,9 @@ data_path = argv[1]
 permutations = ["nest_seed_1", "nest_seed_2", "nest_seed_3",
                 "seed_1_seed_2", "seed_1_seed_3", "seed_2_seed_3"]
 
+permutation_names = ["GeNN 1 vs NEST", "GeNN 2 vs NEST", "GeNN 3 vs NEST",
+                     "GeNN 1 vs GeNN 2", "GeNN 1 vs GeNN 3", "GeNN 2 vs GeNN 3"]
+
 # Population names
 populations = ["4E", "4I", "5E", "5I", "6E", "6I", "23E", "23I"]
 
@@ -47,37 +55,47 @@ for p in permutations:
 # Create second figure to show KL divergence
 kl_fig, kl_axes = plt.subplots(3, frameon=False,
                                figsize=(8.5 * plot_settings.cm_to_inches, 
-                                        5.0 * plot_settings.cm_to_inches))
+                                        6.0 * plot_settings.cm_to_inches))
 
 # Position bars
 kl_bar_width = 0.8
-kl_bar_pad = 0.4
+kl_bar_pad = 0.2
 kl_bar_x = np.arange(0.0, len(populations) * (kl_bar_width + kl_bar_pad), kl_bar_width + kl_bar_pad)
 
 # Plot bars
 permutation_actors = []
 
+# Draw rate KL-divergence bars
 for i, r in enumerate(rate_kl_div):
     permutation_actors.append(kl_axes[0].bar((kl_bar_x * len(permutations)) + (i * kl_bar_width), r, kl_bar_width)[0])
-    
+
+# Draw correlation coefficient KL-divergence bars
 for i, c in enumerate(corr_coeff_kl_div):
     kl_axes[1].bar((kl_bar_x * len(permutations)) + (i * kl_bar_width), c, kl_bar_width)
 
+# Draw irregularity KL-divergence bars
 for i, g in enumerate(irregularity_div):
     kl_axes[2].bar((kl_bar_x * len(permutations)) + (i * kl_bar_width), g, kl_bar_width)
 
 # Set axis labels and titles
 for axis, title in zip(kl_axes, ["A", "B", "C"]):
+    remove_junk(axis)
     axis.set_ylabel("$D_{KL}$")
     axis.set_title(title, loc="left")
-    axis.set_xticks(kl_bar_x * len(permutations))
+    axis.set_xticks((kl_bar_x * len(permutations)) + (kl_bar_width * 2.5))
     axis.set_xticklabels(populations, ha="center")
 
-kl_fig.legend(permutation_actors, permutations,
-              ncol=3, loc="lower center")
+# Fiddle with ticks
+kl_axes[0].set_yticks([0, 0.001, 0.002])
+kl_axes[1].set_yticks([0, 0.002, 0.004])
+kl_axes[2].set_yticks([0, 0.001, 0.002])
 
-#kl_fig.tight_layout(pad=0, rect=(0, 0.15, 1, 1))
-#kl_fig.savefig("../figures/microcircuit_accuracy_kl.eps")
+kl_fig.legend(permutation_actors, permutation_names,
+              ncol=2, loc="lower center")
+
+kl_fig.tight_layout(pad=0, rect=(0, 0.225, 1, 1))
+if not plot_settings.presentation:
+    kl_fig.savefig("../figures/microcircuit_accuracy_kl.pdf")
 
 # Show plot
 plt.show()
